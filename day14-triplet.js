8d33e8bbdc4fd97afea653e78f89edbe14135835
Jessica Sarkar <jessicalosee@gmail.com>
2022-11-15 06:49:31 -0600

    day 15 - triplet

---
+++ day14-triplet.js
@@ -0,0 +1,24 @@
+// You need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers.
+
+// For example:
+
+//an array of 3 numbers
+
+//the index of the number that falls in the middle
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+function gimme (triplet) {
+    const newTrip = triplet.slice()
+    
+    newTrip.sort((a,b) => a-b)
+    return triplet.indexOf(newTrip[1])
+}

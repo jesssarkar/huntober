a65f34754aff20174bc907499b55ee14360d3d7b
Jessica Sarkar <jessicalosee@gmail.com>
2022-11-29 08:14:17 -0600

    day 18 - remove string duplicates

---
+++ day18-stringDup.js
@@ -0,0 +1,21 @@
+// You will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+// Strings will be lowercase only, no spaces 
+
+//an array of strings, all lowercase, no spaces
+
+//return the array with the conesecutive duplicate letters removed
+
+// dup(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"]),['codewars','picaniny','hubububo'])
+// dup(["abracadabra","allottee","assessee"]),['abracadabra','alote','asese'])
+// dup(["kelless","keenness"]), ['keles','kenes'])
+
+function dup(s) {
+    return s.map(x => x.split('').filter((w,i,a) => a[i] != a[i-1]).join(''))
+ 
+ 
+     }
